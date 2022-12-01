9681a8f120f4fa19dc652fc53c5589c777a09f3f
Youssef Doss <youssefdoss97@gmail.com>
2022-11-30 20:02:02 -0500

    finished code review

--- app.py
+++ app.py
@@ -10,5 +10,14 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def home_page():
+    """Renders the home page showing input form"""
 
     return render_template("questions.html", prompts=silly_story.prompts)
+
+@app.get("/results")
+def get_results():
+    """Renders results page based on inputs"""
+    # Make these variables and pull them out of the return
+    return render_template(
+        "results.html",
+        final_story=silly_story.generate(request.args))
